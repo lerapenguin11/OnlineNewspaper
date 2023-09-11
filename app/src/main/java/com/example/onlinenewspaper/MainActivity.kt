aedb2a25ad2d6e8a2dd6a22ae9892809245f6437
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-09-11 17:25:54 +0500

    project setup

--- app/src/main/java/com/example/onlinenewspaper/MainActivity.kt
+++ app/src/main/java/com/example/onlinenewspaper/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.onlinenewspaper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.onlinenewspaper.databinding.ActivityMainBinding
+import com.example.onlinenewspaper.utilits.APP_ACTIVITY
+import com.example.onlinenewspaper.utilits.setStatusBarGradiantMain
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        APP_ACTIVITY = this
+        setStatusBarGradiantMain(this)
+        setContentView(binding.root)
     }
 }
